e989d18f56ef5a50d75458929d11067f3e7d2405
MeYar <107683112+MeYarWebAp@users.noreply.github.com>
2023-01-24 15:06:17 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -21,7 +21,7 @@ plt.style.use('bmh')
 colors = ['#348ABD', '#A60628', '#7A68A6', '#467821', '#D55E00','#CC79A7', '#56B4E9', '#009E73', '#F0E442', '#0072B2']
 st.set_option('deprecation.showPyplotGlobalUse', False)
 onedrive_link ="https://1drv.ms/x/s!AquyG0uXFObDgQXeo9qIu_prTFHx?e=1Behqf"
-onedrive_link_h ="https://1drv.ms/x/s!AquyG0uXFObDgQf0OqCP94Cnnk7Y?e=geT7NQ"
+onedrive_link_h ="https://1drv.ms/x/s!AquyG0uXFObDgQnETyxuqoKnWhJn?e=cVEPIl"
 @st.cache
 def create_onedrive_directdownload (onedrive_link):
     data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
@@ -39,11 +39,11 @@ st.set_page_config(layout="wide")  # this needs to be the first Streamlit comman
 crl_h=create_onedrive_directdownload (onedrive_link_h)
 dataset_h=pd.read_excel(crl_h)
 st.write(dataset_h)
-messages=dataset_h
-ax = messages.groupby('prev_sender')['conversation_id'].size().plot(
-    kind='bar', figsize=(12,3), title='Number of messages sent per recipient', color=colors[0])
-_ = ax.set_xlabel('Previous Sender')
-_ = ax.set_ylabel('Number of messages')
+covidbook=dataset_h
+ax = covidbook.groupby['Response Type'].size().plot(
+    kind='bar', figsize=(12,3), title='Number of implementation done per response', color=colors[0])
+_ = ax.set_xlabel('Response code')
+_ = ax.set_ylabel('Number of implementation')
 _ = plt.xticks(rotation=45)
 st.pyplot()
 indiv_traces = {}

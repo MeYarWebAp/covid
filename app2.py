4e59cfe768a0e175c4c5f91eb652d71331ecfa23
MeYar <107683112+MeYarWebAp@users.noreply.github.com>
2023-01-24 18:11:56 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -21,7 +21,7 @@ plt.style.use('bmh')
 colors = ['#348ABD', '#A60628', '#7A68A6', '#467821', '#D55E00','#CC79A7', '#56B4E9', '#009E73', '#F0E442', '#0072B2']
 st.set_option('deprecation.showPyplotGlobalUse', False)
 onedrive_link ="https://1drv.ms/x/s!AquyG0uXFObDgQXeo9qIu_prTFHx?e=1Behqf"
-onedrive_link_h ="https://1drv.ms/x/s!AquyG0uXFObDgQnETyxuqoKnWhJn?e=cVEPIl"
+onedrive_link_h ="https://1drv.ms/x/s!AquyG0uXFObDgQrnt4QZ_YHZk1uB?e=FTqMBY"
 @st.cache
 def create_onedrive_directdownload (onedrive_link):
     data_bytes64 = base64.b64encode(bytes(onedrive_link, 'utf-8'))
@@ -62,10 +62,10 @@ n_responses = len(responses)
 
 for r in responses:
     with pm.Model() as model:
-        alpha = pm.Uniform('alpha', lower=0, upper=1000)
-        mu = pm.Uniform('mu', lower=0, upper=1000)
+        alpha = pm.Uniform('alpha', lower=0, upper=100)
+        mu = pm.Uniform('mu', lower=0, upper=100)
         
-        data = covidbook[covidbook['Response Type']==r]['daily_deaths'].values
+        data = covidbook[covidbook['Response Type']==r]['Normalized_daily_deaths'].values
         y_est = pm.NegativeBinomial('y_est', mu=mu, alpha=alpha, observed=data)
 
         y_pred = pm.NegativeBinomial('y_pred', mu=mu, alpha=alpha)
@@ -83,17 +83,17 @@ x_lim = 1300
 ix = [3,20,35]
 
 for i, j, r in zip([0,1,2], [0,2,4], responses[ix]):
-    axs[j].set_title('Observed: %s' % p)
-    axs[j].hist(covidbook[covidbook['Response Type']==r]['daily_deaths'].values, range=[0, x_lim], bins=x_lim, histtype='stepfilled')
+    axs[j].set_title('Observed: %s' % r)
+    axs[j].hist(covidbook[covidbook['Response Type']==r]['Normalized_daily_deaths'].values, range=[0, x_lim], bins=x_lim, histtype='stepfilled')
     axs[j].set_ylim([0, y_left_max])
 
 for i, j, r in zip([0,1,2], [1,3,5], responses[ix]):
-    axs[j].set_title('Posterior predictive distribution: %s' % p)
+    axs[j].set_title('Posterior predictive distribution: %s' % r)
     axs[j].hist(indiv_traces[r].get_values('y_pred'), range=[0, x_lim], bins=x_lim, histtype='stepfilled', color=colors[1])
     axs[j].set_ylim([0, y_right_max])
 
-axs[4].set_xlabel('daily_deaths')
-axs[5].set_xlabel('daily_deaths')
+axs[4].set_xlabel('Normalized_daily_deaths')
+axs[5].set_xlabel('Normalized_daily_deaths')
 
 plt.tight_layout()
 st.pyplot()

4bab2786768d937db95839f84d7733c9971262e8
MeYar <107683112+MeYarWebAp@users.noreply.github.com>
2023-03-05 01:05:18 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -18,5 +18,5 @@ lm=['EntertainmentVenuesPartial','RestaurantsCafesPartial','EntertainmentVenues'
 st.multiselect('select a country or a set of countries',lc)
 st.multiselect('select a government response or a set of government responses',lm)
 st.number_input('select number of days per incidence')
-st.selectbox('select a method',['hierarchical method'])
+st.selectbox('select a method',['hierarchical method'],[])
 st.button('posterior predictive analysis')
